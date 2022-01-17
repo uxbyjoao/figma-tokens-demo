f7b70824ca9b1d54ccb3e238089cbbe07bf894c2
João Gomes <jlfgomes@gmail.com>
2022-01-17 17:10:56 +0100

    feat(components): working on button styling

--- src/components/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="App">ayoooooo</div>;
-}
-
-export default App;
---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import Button from "./Button/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
